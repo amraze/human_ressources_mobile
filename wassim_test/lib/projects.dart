4cd503d8a5f758dc90fef3ebefa47d6a4de5bd9a
WassimSellami <wassim.sellami@supcom.tn>
2022-11-18 22:55:03 +0100

    adding card

--- wassim_test/lib/projects.dart
+++ wassim_test/lib/projects.dart
@@ -49,15 +49,16 @@ class _projectsState extends State<projects> {
         ),
       ),
       appBar: 
-   PreferredSize(
-          preferredSize: Size.fromHeight(50.0), // here the desired height
-          child: AppBar(
-         title:Text('Projects'),
+          PreferredSize(
+            preferredSize: Size.fromHeight(50.0), // here the desired height
+            child: AppBar(
+              backgroundColor: Color(0xff2496AC),
+          title:Text('Projects'),
         centerTitle: true,
     )),
     body: 
     Container(
-      padding: const EdgeInsets.all(60),
+      padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
@@ -68,22 +69,91 @@ class _projectsState extends State<projects> {
         )
       ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-
-          
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-            
+            buildImageInteractionCard(),
           ]   
           ),
         ],
       ),
     ));
-
   }
 }
 
+Widget buildQuoteCard() => Card(
+    clipBehavior: Clip.antiAlias,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20)
+    ),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [  
+        Ink.image(
+          image: NetworkImage(
+            'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/256387445_1468760843517912_6354916045760652092_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=BomqUcauA8EAX-L76wR&_nc_oc=AQkFpAwmCDnbvvY3lUQrtsLEiKnogdsyZ9rwQNXj4pvtKPZUe7HYS_PYJnW9aVnHk5E&_nc_ht=scontent.ftun10-1.fna&oh=00_AfCWvCKdU-Lo0wZeNrXttDkWbBOwdFOB8BITtXIY-eXNVQ&oe=637B4882'
+          ),
+          height: 240,
+          fit: BoxFit.cover,
 
+        )
+         
+      ],
+    ),
+);
+
+
+ Widget buildImageInteractionCard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: new InkWell(
+          onTap: () {
+        print("tapped");
+        },
+        child: Column(
+          
+          children: [
+            Stack(
+              
+              children: [
+                Ink.image(
+                  image: NetworkImage(
+                    'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1327&q=80',
+                  ),
+                  height: 190,
+                  width: 360,
+                  fit: BoxFit.fitWidth,
+                ),
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  left: 16,
+                  child: Text(
+                    'Click to show more details',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Project 1',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xff2496AC) ),
+              ),
+            ),
+          ],
+        ),
+        )
+        
+      );
